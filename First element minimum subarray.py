78cb7ebd3d876fa7c1795429c23f5cd1313df6e2
Tushar Mamidwar <tusharmamidwar123@gmail.com>
2022-02-26 20:05:53 +0530

    Completed

--- First element minimum subarray.py
+++ First element minimum subarray.py
@@ -1,1 +1,26 @@
 # Count of Subarrays whose first element is the minimum
+"""
+Given an array arr[] of size N, the task is to find the number of subarrays whose first element 
+is not greater than other elements of the subarray.
+
+Examples:
+
+Input: arr = {1, 2, 1}
+Output: 5
+Explanation: All subarray are: {1}, {1, 2}, {1, 2, 1}, {2}, {2, 1}, {1}
+From above subarray the following meets the condition: {1}, {1, 2}, {1, 2, 1}, {2}, {1}
+
+Input: arr[] = {1, 3, 5, 2}
+Output: 8
+Explanation: We have the following subarrays which meet the condition:
+{1}, {1, 3}, {1, 3, 5}, {1, 3, 5, 2}, {3}, {3, 5}, {5}, {2}
+"""
+count=0
+arr=[1, 3, 5, 2]
+for i in range(len(arr)):
+    subarr=[]
+    for j in range(i,len(arr)):
+        subarr.append(arr[j])
+        if min(subarr)==subarr[0]:
+            count+=1
+print(count)  
--- Sum of odd integers with sum n.py
+++ Sum of odd integers with sum n.py
@@ -1,1 +1,43 @@
+#Count of ways to generate Sequence of distinct consecutive odd integers with sum N
+"""
+Given an integer N,  the task is to find the total number of ways a sequence can be formed consisting of distinct consecutive odd integers that add up to N.
 
+Examples:
+
+Input: N = 45
+Output: 3
+Explanation: 3 ways to choose distinct consecutive odd numbers that add up to 45 are – 
+{5, 7, 9, 11, 13}, {13, 15, 17} and {45}.
+
+Input : N = 20
+Output : 1
+Explanation: 9 and 11 are the only consecutive odd numbers whose sum is 20
+"""
+import math
+N=int(input())
+
+#Naive Approach TC=O^2
+print("Naive Approach:", end="")
+i=1
+count=0
+while i<=N:
+    sum=0
+    j=i
+    while sum<N:
+        sum+=j
+        j+=2
+    if sum==N:
+        count+=1
+    i+=2
+print(count)
+
+#Better Approach TC=O√n
+print("Better Approach:", end="")
+count=0
+i=1
+nsqrt=math.sqrt(N)
+while i<=nsqrt: #based on finding all the divisors of a number
+    if N%i==0 and (N/i+i)%2==0: #N/i and i are the two divisors whose sum should be even
+        count+=1
+    i+=1
+print(count)
